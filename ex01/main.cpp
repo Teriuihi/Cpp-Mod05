50af8c89716172705716f4a739404d5f84e05652
Stijn Appunn <sappunn@student.codam.nl>
2023-01-09 18:51:47 +0100

    Added more tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -11,8 +11,24 @@ void printGrade(Bureaucrat &bureaucrat) {
 
 int main() {
 	try {
-		Bureaucrat b1 = Bureaucrat("test b 1", 30);
-		Form f1 = Form("test form 1", 30, 30);
+		Bureaucrat b1 = Bureaucrat("b30", 30);
+		Form f1 = Form("f30", 30, 30);
+		b1.signForm(f1);
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	try {
+		Bureaucrat b1 = Bureaucrat("b30", 30);
+		Form f1 = Form("f20", 20, 20);
+		b1.signForm(f1);
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	try {
+		Bureaucrat b1 = Bureaucrat("b30", 30);
+		Form f1 = Form("f40", 40, 40);
 		b1.signForm(f1);
 	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
