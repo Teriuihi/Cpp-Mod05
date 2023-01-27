b59b9b85121d3c4d372a2e1f67bcce02bae11c79
Stijn Appunn <sappunn@student.codam.nl>
2023-01-27 16:55:49 +0100

    Added better tests

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,9 +1,24 @@
 #include "Intern.hpp"
+#include "AForm.hpp"
+
+void runForm(AForm *form, Bureaucrat &bureaucrat) {
+	if (form == NULL)
+		return;
+	form->beSigned(bureaucrat);
+	form->execute(bureaucrat);
+}
 
 int main() {
-	Intern().makeForm("presidential pardon form", "One");
-	Intern().makeForm("robotomy request form", "Two");
-	Intern().makeForm("shrubbery creation form", "Three");
-	Intern().makeForm("shrubbery creation form ", "Four");
+	Bureaucrat bob = Bureaucrat("bob", 1);
+	Intern steve = Intern();
+
+	try {
+		runForm(steve.makeForm("presidential pardon form", "One"), bob);
+		runForm(steve.makeForm("robotomy request form", "Two"), bob);
+		runForm(steve.makeForm("shrubbery creation form", "Three"), bob);
+		runForm(steve.makeForm("shrubbery creation form ", "Four"), bob);
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 	return 0;
 }
